f5b830ac42e8f1ed27c09bea1a0db7592a86aee1
Derek M Frank <derek@frank.sh>
2024-10-20 09:49:29 -0700

    feat(n8n.frank.sh): Deploy basic n8n

---
+++ terraform/applications/n8n/README.md
@@ -0,0 +1,13 @@
+# n8n
+
+Low-code, workflow automation tool.
+
+**Resources:**
+
+- [Self-hosting n8n][n8n-self-hosting]
+- [n8n | ArtifactHub][artifacthub-n8n]
+
+---
+
+[artifacthub-n8n]: https://artifacthub.io/packages/helm/open-8gears/n8n
+[n8n-self-hosting]: https://docs.n8n.io/hosting/
---
+++ terraform/applications/n8n/n8n.tf
@@ -0,0 +1,105 @@
+variable "kubeconfig" {
+  type = string
+}
+
+terraform {
+  backend "s3" {
+    bucket                      = "frankenstructure"
+    key                         = "n8n/production/terraform.tfstate"
+    endpoint                    = "sjc1.vultrobjects.com"
+    region                      = "us-west-1"
+    skip_credentials_validation = true
+  }
+  required_version = ">= 1.5.7"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+provider "kubernetes" {
+  config_path = var.kubeconfig
+}
+
+# https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig
+  }
+}
+
+resource "kubernetes_namespace" "n8n" {
+  metadata {
+    name = "n8n"
+  }
+}
+
+# https://artifacthub.io/packages/helm/open-8gears/n8n
+resource "helm_release" "n8n" {
+  name       = "n8n"
+  repository = "oci://8gears.container-registry.com/library"
+  chart      = "n8n"
+  version    = "0.25.2"
+  namespace  = kubernetes_namespace.n8n.metadata[0].name
+
+  set {
+    name  = "extraEnv.WEBHOOK_TUNNEL_URL"
+    value = "https://n8n.frank.sh/"
+  }
+
+  set {
+    name  = "generic.timezone"
+    value = "America/Los_Angeles"
+  }
+}
+
+resource "kubernetes_manifest" "n8n_route" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind"       = "HTTPRoute"
+    "metadata" = {
+      "name"      = "n8n"
+      "namespace" = helm_release.n8n.namespace
+    }
+    "spec" = {
+      "parentRefs" = [
+        {
+          "kind"        = "Gateway"
+          "name"        = "prod-web"
+          "namespace"   = "nginx-gateway"
+          "sectionName" = "https-wildcard.frank.sh"
+        }
+      ]
+      "hostnames" = [
+        "n8n.frank.sh",
+      ]
+      "rules" = [
+        {
+          "matches" = [
+            {
+              "path" = {
+                "type"  = "PathPrefix"
+                "value" = "/"
+              }
+            }
+          ]
+          "backendRefs" = [
+            {
+              "kind"      = "Service"
+              "name"      = helm_release.n8n.name
+              "namespace" = helm_release.n8n.namespace
+              "port"      = 80
+            }
+          ]
+        }
+      ]
+    }
+  }
+}

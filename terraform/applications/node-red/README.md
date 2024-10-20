6cdadcc05436a5d98f00886033d2d69d72536703
Derek M Frank <derek@frank.sh>
2024-10-20 09:50:37 -0700

    feat(nodered.frank.sh): Deploy basic Node-RED

---
+++ terraform/applications/node-red/README.md
@@ -0,0 +1,13 @@
+# Node-RED
+
+Low-code, workflow automation tool.
+
+**Resources:**
+
+- [Node-RED Install][nodered-install]
+- [Node-RED | ArtifactHub][artifacthub-nodered]
+
+---
+
+[artifacthub-nodered]: https://artifacthub.io/packages/helm/node-red/node-red
+[nodered-install]: https://nodered.org/docs/getting-started/
---
+++ terraform/applications/node-red/node-red.tf
@@ -0,0 +1,122 @@
+variable "github_username" {
+  type = string
+}
+
+variable "github_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "kubeconfig" {
+  type = string
+}
+
+terraform {
+  backend "s3" {
+    bucket                      = "frankenstructure"
+    key                         = "node-red/production/terraform.tfstate"
+    endpoint                    = "sjc1.vultrobjects.com"
+    region                      = "us-west-1"
+    skip_credentials_validation = true
+  }
+  required_version = ">= 1.5.7"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+provider "kubernetes" {
+  config_path = var.kubeconfig
+}
+
+# https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig
+  }
+
+  registry {
+    url      = "oci://ghcr.io"
+    username = var.github_username
+    password = var.github_token
+  }
+}
+
+resource "kubernetes_namespace" "nodered" {
+  metadata {
+    name = "node-red"
+  }
+}
+
+# https://artifacthub.io/packages/helm/node-red/node-red
+resource "helm_release" "nodered" {
+  name       = "node-red"
+  repository = "oci://ghcr.io/schwarzit/charts"
+  chart      = "node-red"
+  version    = "0.33.0"
+  namespace  = kubernetes_namespace.nodered.metadata[0].name
+
+  values = [
+    yamlencode({
+      "env" = [
+        {
+          "name"  = "TZ"
+          "value" = "America/Los_Angeles"
+        },
+      ]
+    }),
+  ]
+}
+
+resource "kubernetes_manifest" "nodered_route" {
+  manifest = {
+    "apiVersion" = "gateway.networking.k8s.io/v1"
+    "kind"       = "HTTPRoute"
+    "metadata" = {
+      "name"      = "node-red"
+      "namespace" = helm_release.nodered.namespace
+    }
+    "spec" = {
+      "parentRefs" = [
+        {
+          "kind"        = "Gateway"
+          "name"        = "prod-web"
+          "namespace"   = "nginx-gateway"
+          "sectionName" = "https-wildcard.frank.sh"
+        }
+      ]
+      "hostnames" = [
+        "node-red.frank.sh",
+        "nodered.frank.sh",
+      ]
+      "rules" = [
+        {
+          "matches" = [
+            {
+              "path" = {
+                "type"  = "PathPrefix"
+                "value" = "/"
+              }
+            }
+          ]
+          "backendRefs" = [
+            {
+              "kind"      = "Service"
+              "name"      = helm_release.nodered.name
+              "namespace" = helm_release.nodered.namespace
+              "port"      = 1880
+            }
+          ]
+        }
+      ]
+    }
+  }
+}

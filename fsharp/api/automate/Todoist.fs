25bdda74ddea1aed458308cbe05073abf43f9b39
Derek M Frank <derek@frank.sh>
2024-11-11 10:09:15 -0800

    fix(automate-api): Parse and route Todoist comment events

--- fsharp/api/automate/Todoist.fs
+++ fsharp/api/automate/Todoist.fs
@@ -1,93 +1,91 @@
 module AutoMate.Todoist
 
 open System
+open System.Text.Json
+open System.Text.Json.Serialization
 
 open Falco
+
+let jsonOptions =
+  // https://learn.microsoft.com/en-us/dotnet/api/system.text.json.jsonserializeroptions
+  let options = JsonFSharpOptions.Default().ToJsonSerializerOptions()
+  options.AllowTrailingCommas <- true
+  // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/customize-properties?pivots=dotnet-8-0#enums-as-strings
+  JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower)
+  |> options.Converters.Add
+
+  options.PropertyNameCaseInsensitive <- true
+  // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/customize-properties?pivots=dotnet-8-0#use-a-custom-json-property-naming-policy
+  options.PropertyNamingPolicy <- JsonNamingPolicy.SnakeCaseLower
+  options.ReadCommentHandling <- JsonCommentHandling.Skip
+  // TODO: options.WriteIndented <- true
+  options
 
 (*
 BLOCK_LINE = re.compile(r"\s[-*] ")
 
 
-class CommentData(pydantic.BaseModel):
-    id: str
-    url: pydantic.AnyHttpUrl
-    content: str
-    is_deleted: bool
-    task_id: str = pydantic.Field(alias="item_id")
-    task: Task | None = pydantic.Field(None, alias="item")
-    posted_at: dt.datetime
+"""
+https://developer.todoist.com/sync/v9/#request-format
 
-
-class TodoistEvent(pydantic.BaseModel):
-    """
-    https://developer.todoist.com/sync/v9/#request-format
-
-    json = {
-        "triggered_at": "2024-10-30T13:56:25.601532Z",
-        "initiator": {
-            "id": "40102343",
-            "email": "derek+todoist@frank.sh",
-            "image_id": "86d065186f7a4073af86a5b4921e97b4",
-            "full_name": "Derek",
-            "is_premium": true
-        },
-        "event_name": "note:added",
-        "event_data": {
-            "id": "3651431408",
-            "url": "https://app.todoist.com/app/task/8283578355",
-            "item": {
-                "id": "8283578355",
-                "due": null,
-                "v2_id": "6Vx3G2XJcJPxfvcG",
-                "labels": [],
-                "checked": false,
-                "content": "Sync new comments from Todoist to Logseq",
-                "sync_id": null,
-                "user_id": "40102343",
-                "added_at": "2024-08-10T17:12:50.068600Z",
-                "deadline": null,
-                "duration": null,
-                "priority": 4,
-                "collapsed": false,
-                "parent_id": null,
-                "is_deleted": false,
-                "project_id": "2337670886",
-                "section_id": "163355751",
-                "updated_at": "2024-10-19T13:38:58Z",
-                "child_order": 2,
-                "description": "TBD: where are comments stored in Logseq?\n\nBlocked: on infrastructure setup",
-                "added_by_uid": "40102343",
-                "completed_at": null,
-                "v2_parent_id": null,
-                "v2_project_id": "6Vx3Cf3QvmqjR7vG",
-                "v2_section_id": "6Vx3FmfM9xJQwqrp",
-                "assigned_by_uid": null,
-                "responsible_uid": null
-            },
-            "v2_id": "6WVxFjgm4pPpPMGG",
-            "content": "test 6",
-            "item_id": "8283578355",
-            "posted_at": "2024-10-30T13:56:24.766000Z",
-            "reactions": null,
+json = {
+    "triggered_at": "2024-10-30T13:56:25.601532Z",
+    "initiator": {
+        "id": "40102343",
+        "email": "derek+todoist@frank.sh",
+        "image_id": "86d065186f7a4073af86a5b4921e97b4",
+        "full_name": "Derek",
+        "is_premium": true
+    },
+    "event_name": "note:added",
+    "event_data": {
+        "id": "3651431408",
+        "url": "https://app.todoist.com/app/task/8283578355",
+        "item": {
+            "id": "8283578355",
+            "due": null,
+            "v2_id": "6Vx3G2XJcJPxfvcG",
+            "labels": [],
+            "checked": false,
+            "content": "Sync new comments from Todoist to Logseq",
+            "sync_id": null,
+            "user_id": "40102343",
+            "added_at": "2024-08-10T17:12:50.068600Z",
+            "deadline": null,
+            "duration": null,
+            "priority": 4,
+            "collapsed": false,
+            "parent_id": null,
             "is_deleted": false,
-            "posted_uid": "40102343",
-            "v2_item_id": "6Vx3G2XJcJPxfvcG",
+            "project_id": "2337670886",
+            "section_id": "163355751",
+            "updated_at": "2024-10-19T13:38:58Z",
+            "child_order": 2,
+            "description": "TBD: where are comments stored in Logseq?\n\nBlocked: on infrastructure setup",
+            "added_by_uid": "40102343",
+            "completed_at": null,
+            "v2_parent_id": null,
             "v2_project_id": "6Vx3Cf3QvmqjR7vG",
-            "uids_to_notify": null,
-            "file_attachment": null
+            "v2_section_id": "6Vx3FmfM9xJQwqrp",
+            "assigned_by_uid": null,
+            "responsible_uid": null
         },
-        "user_id": "40102343",
-        "version": "9"
-    }
-    """
-    version: str
-    user_id: str
-    triggered_at: dt.datetime
-    initiator: EventInitiator
-    event_name: EventName
-    event_data: CommentData
-
-
+        "v2_id": "6WVxFjgm4pPpPMGG",
+        "content": "test 6",
+        "item_id": "8283578355",
+        "posted_at": "2024-10-30T13:56:24.766000Z",
+        "reactions": null,
+        "is_deleted": false,
+        "posted_uid": "40102343",
+        "v2_item_id": "6Vx3G2XJcJPxfvcG",
+        "v2_project_id": "6Vx3Cf3QvmqjR7vG",
+        "uids_to_notify": null,
+        "file_attachment": null
+    },
+    "user_id": "40102343",
+    "version": "9"
+}
+"""
 def main(event: dict) -> dict:
     """
     https://developer.todoist.com/appconsole.html
@@ -146,37 +144,18 @@ def get_task(comment_event: TodoistEvent) -> Task:
 type Initiator = {
   Id: string
   Email: string
-  FullName: string // TODO: full_name
+  FullName: string
 }
-
-// https://developer.todoist.com/sync/v9/#webhooks
-type EventName =
-  | CommentAdded
-  | CommentUpdated
-  | CommentDeleted
-
-  member this.AsString() =
-    match this with
-    | CommentAdded -> "note:added"
-    | CommentUpdated -> "note:updated"
-    | CommentDeleted -> "note:deleted"
-
-  static member FromString(s) =
-    match s with
-    | "note:added" -> Some CommentAdded
-    | "note:updated" -> Some CommentUpdated
-    | "note:deleted" -> Some CommentDeleted
-    | _ -> None
 
 type Task = {
   Id: string
-  ParentId: string option // TODO: parent_id
-  SectionId: string option // TODO: section_id
-  ProjectId: string // TODO: project_id
+  ParentId: string option
+  SectionId: string option
+  ProjectId: string
   Priority: int
   Labels: int list
-  IsDeleted: bool // TODO: is_deleted
-  CompletedAt: DateTime option // TODO: completed_at
+  IsDeleted: bool
+  CompletedAt: DateTime option
   Content: string
   Description: string
 }
@@ -184,19 +163,20 @@ type Task = {
 type EventData = {
   Id: string
   Url: string
-  IsDeleted: bool // TODO: is_deleted
-  TaskId: string // TODO: item_id
-  Task: Task option
-  PostedAt: DateTime // TODO: posted_at
+  IsDeleted: bool
+  TaskId: string
+  //Task: Task option
+  PostedAt: DateTime
 }
 
 type WebhookEvent = {
   Version: string
   UserId: string
-  TriggeredAt: DateTime // TODO: triggered_at
+  TriggeredAt: DateTime
   Initiator: Initiator
-  EventName: EventName // TODO: event_name
-  EventData: EventData // TODO: event_data
+  // https://developer.todoist.com/sync/v9/#webhooks
+  EventName: string
+  EventData: EventData
 }
 
 module WebhookEvent =
@@ -209,16 +189,18 @@ module WebhookEvent =
 
   let handler: HttpHandler =
     fun ctx ->
-      let handleOk event : HttpHandler =
+      let handleOk (event: WebhookEvent) : HttpHandler =
+        printfn "%A" event
+
         let result =
           match event with
-          | { EventName = EventName.CommentAdded } -> success
-          | { EventName = EventName.CommentUpdated } -> success
-          | { EventName = EventName.CommentDeleted } -> unsupported
-          | _ -> failure
+          | { EventName = "note:added" } -> success
+          | { EventName = "note:updated" } -> success
+          | { EventName = "note:deleted" } -> unsupported
+          | _ -> unsupported
 
         // Response.ofJson {}
         // Response.ofJson Seq.empty
         Response.ofJson result
 
-      Request.mapJson handleOk ctx
+      Request.mapJsonOption jsonOptions handleOk ctx

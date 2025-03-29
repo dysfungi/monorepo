b86c2a4cc21f37ca5456d52b5608819b2c76a7cf
Derek M Frank <derek@frank.sh>
2025-03-29 15:58:01 -0700

    chore!(vultr): Remove deprecated VPC 2.0
    https://docs.vultr.com/how-to-migrate-from-vultr-vpc-20-network-to-a-vultr-vpc-network

--- terraform/infrastructure/frankenstructure/vultr_vpc.tf
+++ terraform/infrastructure/frankenstructure/vultr_vpc.tf
@@ -1,8 +1,3 @@
-resource "vultr_vpc2" "frankenetwork" {
-  description = "frankenetwork"
-  region      = "lax"
-}
-
 resource "vultr_vpc" "k8s" {
   # id = "0dcffa14-ac8e-49cb-8710-3dcc46a97f1f"
   region = "lax"

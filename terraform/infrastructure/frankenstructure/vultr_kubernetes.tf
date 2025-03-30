bd83c715c913a05e1f5e8556a8753ac4b233c228
Derek M Frank <derek@frank.sh>
2025-03-29 20:03:20 -0700

    refactor(vultr-k8s): Move hard-coded plan IDs to local variables

--- terraform/infrastructure/frankenstructure/vultr_kubernetes.tf
+++ terraform/infrastructure/frankenstructure/vultr_kubernetes.tf
@@ -1,3 +1,19 @@
+locals {
+  plans = {
+    cloud_compute = {
+      intel_oldgen_10usd = "vc2-1c-2gb"
+      intel_oldgen_15usd = "vc2-2c-2gb"
+      intel_oldgen_20usd = "vc2-2c-4gb"
+      intel_oldgen_40usd = "vc2-4c-8gb"
+    }
+    cloud_gpu = {
+      nvidia_a16_40usd = "vcg-a16-2c-8g-2vram" # NOTE: Was unavailable for VKE on 2025-03-29
+      nvidia_a40_55usd = "vcg-a40-1c-5g-2vram"
+    }
+  }
+
+}
+
 resource "vultr_kubernetes" "k8s" {
   region           = "lax"
   label            = "frank8s"
@@ -7,7 +23,7 @@ resource "vultr_kubernetes" "k8s" {
 
   node_pools {
     node_quantity = 2
-    plan          = "vc2-2c-4gb"
+    plan          = local.plans.cloud_compute.intel_oldgen_20usd
     label         = "default"
     auto_scaler   = true
     min_nodes     = 2
@@ -18,7 +34,7 @@ resource "vultr_kubernetes" "k8s" {
 resource "vultr_kubernetes_node_pools" "gateway" {
   cluster_id    = vultr_kubernetes.k8s.id
   node_quantity = 1
-  plan          = "vc2-1c-2gb"
+  plan          = local.plans.cloud_compute.intel_oldgen_10usd
   label         = "gateway"
   tag           = "gateway"
   auto_scaler   = true
@@ -29,7 +45,7 @@ resource "vultr_kubernetes_node_pools" "gateway" {
 resource "vultr_kubernetes_node_pools" "monitoring" {
   cluster_id    = vultr_kubernetes.k8s.id
   node_quantity = 1
-  plan          = "vc2-2c-2gb"
+  plan          = local.plans.cloud_compute.intel_oldgen_15usd
   label         = "monitoring"
   tag           = "monitoring"
   auto_scaler   = true
@@ -40,11 +56,10 @@ resource "vultr_kubernetes_node_pools" "monitoring" {
 resource "vultr_kubernetes_node_pools" "llm" {
   cluster_id    = vultr_kubernetes.k8s.id
   node_quantity = 1
-  # plan          = "vcg-a16-2c-8g-2vram"
-  plan        = "vc2-4c-8gb"
-  label       = "llm"
-  tag         = "llm"
-  auto_scaler = false
-  min_nodes   = 1
-  max_nodes   = 2
+  plan          = local.plans.cloud_compute.intel_oldgen_40usd
+  label         = "llm"
+  tag           = "llm"
+  auto_scaler   = false
+  min_nodes     = 1
+  max_nodes     = 2
 }

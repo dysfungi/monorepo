8b93b2e03c54e8fa4ca2c1de0c3c78c0ee70e82e
Derek M Frank <dfrank@riotgames.com>
2025-04-09 10:25:02 -0700

    chore(miniflux): Update probe URL to healthcheck endpoint

--- terraform/applications/miniflux/probes.tf
+++ terraform/applications/miniflux/probes.tf
@@ -4,5 +4,5 @@ module "probe" {
   kubeconfig_path      = var.kubeconfig_path
   kubernetes_namespace = local.namespace
   probe_name           = helm_release.miniflux.name
-  probe_url            = "http://${module.route.primary_hostname}/"
+  probe_url            = "http://${module.route.primary_hostname}/healthcheck"
 }

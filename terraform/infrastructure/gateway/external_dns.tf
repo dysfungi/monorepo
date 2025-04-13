7fe8f08f51d1dd83910839954737e46105d99014
Derek M Frank <derek@frank.sh>
2025-04-13 11:01:15 -0700

    chore(gateway): Upgrade external-dns Helm chart

--- terraform/infrastructure/gateway/external_dns.tf
+++ terraform/infrastructure/gateway/external_dns.tf
@@ -5,18 +5,18 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns"
   chart      = "external-dns"
-  version    = "1.15.0"
+  version    = "1.16.1"
   namespace  = kubernetes_namespace.gateway.metadata[0].name
 
   values = [
     yamlencode({
-      "namespaced" = false
-      "provider"   = "cloudflare"
-      "affinity"   = local.affinity
-      "rbac" = {
-        "create" = true
+      namespaced = false
+      provider   = "cloudflare"
+      affinity   = local.affinity
+      rbac = {
+        create = true
       }
-      "sources" = [
+      sources = [
         "gateway-grpcroute",
         "gateway-httproute",
         # "gateway-tcproute",
@@ -25,20 +25,20 @@ resource "helm_release" "external_dns" {
         # "ingress",
         "service",
       ]
-      "env" = [
+      env = [
         {
-          "name" = "CF_API_TOKEN"
-          "valueFrom" = {
-            "secretKeyRef" = {
-              "key"      = "apiToken"
-              "name"     = kubernetes_secret.cloudflare.metadata[0].name
-              "optional" = false
+          name = "CF_API_TOKEN"
+          valueFrom = {
+            secretKeyRef = {
+              key      = "apiToken"
+              name     = kubernetes_secret.cloudflare.metadata[0].name
+              optional = false
             }
           }
         }
       ]
-      "serviceMonitor" = {
-        "enabled" = true
+      serviceMonitor = {
+        enabled = true
       }
     })
   ]

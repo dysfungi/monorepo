c155aaa18b37ccf957bd2f910b4ca5310c414072
Derek M Frank <derek@frank.sh>
2025-04-13 13:09:13 -0700

    fix(automate-api): Change JSON content type header charset parameter to encoding

--- fsharp/api/automate/AutoMate/FalcoExtensions.fs
+++ fsharp/api/automate/AutoMate/FalcoExtensions.fs
@@ -7,5 +7,5 @@ open System.Text
 module Response =
   let myOfJson (obj: 'T) : HttpHandler =
     // https://github.com/pimbrouwers/Falco/blob/25d828d832c0fde2dfff04775bea1eced9050458/src/Falco/Response.fs#L200
-    Response.withContentType "applicaton/json; charset=utf-8"
+    Response.withContentType "applicaton/json; encoding=utf-8"
     >> (Json.serialize >> Response.ofString Encoding.UTF8) obj

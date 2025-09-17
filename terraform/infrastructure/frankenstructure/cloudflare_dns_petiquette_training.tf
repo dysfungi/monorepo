fe9a53fb7b48661c0c6aa6022fb201f2704e3414
Derek M Frank <derek@frank.sh>
2025-09-16 20:10:55 -0700

    fix(cloudflare-dns): Remove Petiquette domains

--- terraform/infrastructure/frankenstructure/cloudflare_dns_petiquette_training.tf
+++
@@ -1,6 +0,0 @@
-# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/zone
-resource "cloudflare_zone" "training_petiquette" {
-  account_id = data.cloudflare_accounts.dmf.id
-  # account_id = resource.cloudflare_account.frankenstructure.id
-  zone = "petiquette.training"
-}
